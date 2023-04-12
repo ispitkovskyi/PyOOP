fba88fed1142818886af515e35fc491b49770d6f
ispitkovskyi <spitkovskyigor@gmail.com>
2023-04-12 19:10:57 +0300

    experiments with multiple inheritance

--- classes_and_objects/inheritance/multiple_inheritances.py
+++ classes_and_objects/inheritance/multiple_inheritances.py
@@ -7,12 +7,16 @@ class Base1(object):
         self.str1 = "Geek1"
         print("Base1")
 
+    def printStr1(self):
+        print(self.str1)
 
 class Base2(object):
     def __init__(self):
         self.str2 = "Geek2"
         print("Base2")
 
+    def printStr2(self):
+        print(self.str2)
 
 class Derived(Base1, Base2):
     def __init__(self):
@@ -24,6 +28,11 @@ class Derived(Base1, Base2):
     def printStrs(self):
         print(self.str1, self.str2)
 
+    def printAllStrs(self):
+        Base1.printStr1(self)
+        Base2.printStr2(self)
+
 
 ob = Derived()
 ob.printStrs()
+ob.printAllStrs()
